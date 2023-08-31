b0183f3ccaa561a983d4b634b237eaa897558d10
ars <terechny@gmail.com>
2023-08-31 10:02:57 +0300

    init

---
+++ mymath.go
@@ -0,0 +1,31 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Sin(x float64) float64 {
+	return math.Sin(x)
+}
+
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+
+func Tan(x float64) float64 {
+	return math.Tan(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
